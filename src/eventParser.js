eb524992f32c9dd39bf83baa85c8c3f1fc99b24a
Jonathan Stones <22222506+Jonathan-Stones@users.noreply.github.com>
2022-10-05 21:43:58 -0600

    first commit

--- src/eventParser.js
+++ src/eventParser.js
@@ -1,21 +1,15 @@
-const avro = require("avro-js");
+const avro = require('avro-js');
 
 function parseEvent(schema, event) {
   const allFields = schema.type.getFields();
   const replayId = event.replayId.readBigUInt64BE().toString();
   const payload = schema.type.fromBuffer(event.event.payload); // This schema is the same which we retreived earlier in the GetSchema rpc.
-  payload.ChangeEventHeader.nulledFields = parseFieldBitmaps(
-    allFields,
-    payload.ChangeEventHeader.nulledFields
-  );
-  payload.ChangeEventHeader.diffFields = parseFieldBitmaps(
-    allFields,
-    payload.ChangeEventHeader.diffFields
-  );
-  payload.ChangeEventHeader.changedFields = parseFieldBitmaps(
-    allFields,
-    payload.ChangeEventHeader.changedFields
-  );
+  // ChangeEventHeader is only present on Change Data Capture Events
+  if (payload.ChangeEventHeader) {
+    payload.ChangeEventHeader.nulledFields = parseFieldBitmaps(allFields, payload.ChangeEventHeader.nulledFields);
+    payload.ChangeEventHeader.diffFields = parseFieldBitmaps(allFields, payload.ChangeEventHeader.diffFields);
+    payload.ChangeEventHeader.changedFields = parseFieldBitmaps(allFields, payload.ChangeEventHeader.changedFields);
+  }
   return {
     replayId,
     payload,
@@ -34,28 +28,19 @@ function parseFieldBitmaps(allFields, fieldBitmapsAsHex) {
   }
   let fieldNames = [];
   // Replace top field level bitmap with list of fields
-  if (fieldBitmapsAsHex[0].startsWith("0x")) {
-    fieldNames = fieldNames.concat(
-      getFieldNamesFromBitmap(allFields, fieldBitmapsAsHex[0])
-    );
+  if (fieldBitmapsAsHex[0].startsWith('0x')) {
+    fieldNames = fieldNames.concat(getFieldNamesFromBitmap(allFields, fieldBitmapsAsHex[0]));
   }
   // Process compound fields
-  if (fieldBitmapsAsHex[fieldBitmapsAsHex.length - 1].indexOf("-") !== -1) {
-    fieldBitmapsAsHex.forEach((fieldBitmapAsHex) => {
-      const bitmapMapStrings = fieldBitmapAsHex.split("-");
+  if (fieldBitmapsAsHex[fieldBitmapsAsHex.length - 1].indexOf('-') !== -1) {
+    fieldBitmapsAsHex.forEach(fieldBitmapAsHex => {
+      const bitmapMapStrings = fieldBitmapAsHex.split('-');
       // Ignore top level field bitmap
       if (bitmapMapStrings.length >= 2) {
         const parentField = allFields[parseInt(bitmapMapStrings[0])];
         const childFields = getChildFields(parentField);
-        const childFieldNames = getFieldNamesFromBitmap(
-          childFields,
-          bitmapMapStrings[1]
-        );
-        fieldNames = fieldNames.concat(
-          childFieldNames.map(
-            (fieldName) => `${parentField._name}.${fieldName}`
-          )
-        );
+        const childFieldNames = getFieldNamesFromBitmap(childFields, bitmapMapStrings[1]);
+        fieldNames = fieldNames.concat(childFieldNames.map(fieldName => `${parentField._name}.${fieldName}`));
       }
     });
   }
@@ -65,7 +50,7 @@ function parseFieldBitmaps(allFields, fieldBitmapsAsHex) {
 function getChildFields(parentField) {
   const types = parentField._type.getTypes();
   let fields = [];
-  types.forEach((type) => {
+  types.forEach(type => {
     if (type instanceof avro.types.RecordType) {
       fields = fields.concat(type.getFields());
     } else if (type instanceof avro.types.NullType) {
@@ -86,7 +71,7 @@ function getFieldNamesFromBitmap(fields, fieldBitmapAsHex) {
   // Use bitmap to figure out field names based on index
   const fieldNames = [];
   for (let i = 0; i < binValue.length; i++) {
-    if (binValue[i] === "1") {
+    if (binValue[i] === '1') {
       fieldNames.push(fields[i].getName());
     }
   }
@@ -94,7 +79,7 @@ function getFieldNamesFromBitmap(fields, fieldBitmapAsHex) {
 }
 
 function reverseBytes(input) {
-  let output = "";
+  let output = '';
   for (let i = input.length / 8 - 1; i >= 0; i--) {
     output += input.substring(i * 8, (i + 1) * 8);
   }
@@ -108,22 +93,22 @@ function reverseBytes(input) {
  */
 function hexToBin(hex) {
   let bin = hex.substring(2); // Remove 0x prefix
-  bin = bin.replaceAll("0", "0000");
-  bin = bin.replaceAll("1", "0001");
-  bin = bin.replaceAll("2", "0010");
-  bin = bin.replaceAll("3", "0011");
-  bin = bin.replaceAll("4", "0100");
-  bin = bin.replaceAll("5", "0101");
-  bin = bin.replaceAll("6", "0110");
-  bin = bin.replaceAll("7", "0111");
-  bin = bin.replaceAll("8", "1000");
-  bin = bin.replaceAll("9", "1001");
-  bin = bin.replaceAll("A", "1010");
-  bin = bin.replaceAll("B", "1011");
-  bin = bin.replaceAll("C", "1100");
-  bin = bin.replaceAll("D", "1101");
-  bin = bin.replaceAll("E", "1110");
-  bin = bin.replaceAll("F", "1111");
+  bin = bin.replaceAll('0', '0000');
+  bin = bin.replaceAll('1', '0001');
+  bin = bin.replaceAll('2', '0010');
+  bin = bin.replaceAll('3', '0011');
+  bin = bin.replaceAll('4', '0100');
+  bin = bin.replaceAll('5', '0101');
+  bin = bin.replaceAll('6', '0110');
+  bin = bin.replaceAll('7', '0111');
+  bin = bin.replaceAll('8', '1000');
+  bin = bin.replaceAll('9', '1001');
+  bin = bin.replaceAll('A', '1010');
+  bin = bin.replaceAll('B', '1011');
+  bin = bin.replaceAll('C', '1100');
+  bin = bin.replaceAll('D', '1101');
+  bin = bin.replaceAll('E', '1110');
+  bin = bin.replaceAll('F', '1111');
   return bin;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 // This project is derived from a [blog post](https://jungleeforce.com/2021/11/11/connecting-to-salesforce-using-pub-sub-api-grpc/) from techinjungle.
 // Official Pub/Sub API repo: https://github.com/developerforce/pub-sub-api
 
-const grpc = require("@grpc/grpc-js");
-const protoLoader = require("@grpc/proto-loader");
-const fs = require("fs");
-const jsforce = require("jsforce");
-const avro = require("avro-js");
-const certifi = require("certifi");
-const { parseEvent } = require("./eventParser.js");
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const fs = require('fs');
+const avro = require('avro-js');
+const certifi = require('certifi');
+const { parseEvent } = require('./eventParser.js');
 
-const authLib = require("../lib/salesforceAuth.js");
-const config = require("../config.json");
+const authLib = require('../lib/salesforceAuth.js');
+const config = require('../config.json');
 
 // Load config from .env file
-require("dotenv").config();
-const {
-  PUB_SUB_ENDPOINT,
-  PUB_SUB_PROTO_FILE,
-  PUB_SUB_TOPIC_NAME,
-  PUB_SUB_EVENT_RECEIVE_LIMIT,
-} = process.env;
+require('dotenv').config();
+const { PUB_SUB_ENDPOINT, PUB_SUB_PROTO_FILE, PUB_SUB_TOPIC_NAME, PUB_SUB_EVENT_RECEIVE_LIMIT } = process.env;
 
 /**
  * Connects to the Pub/Sub API and returns a gRPC client
@@ -38,16 +32,13 @@ function connectToPubSubApi(authParams) {
   // Prepare gRPC connection
   const metaCallback = (_params, callback) => {
     const meta = new grpc.Metadata();
-    meta.add("accesstoken", authParams.accessToken);
-    meta.add("instanceurl", authParams.instanceUrl);
-    meta.add("tenantid", authParams.tenantid);
+    meta.add('accesstoken', authParams.accessToken);
+    meta.add('instanceurl', authParams.instanceUrl);
+    meta.add('tenantid', authParams.tenantId);
     callback(null, meta);
   };
   const callCreds = grpc.credentials.createFromMetadataGenerator(metaCallback);
-  const combCreds = grpc.credentials.combineChannelCredentials(
-    grpc.credentials.createSsl(rootCert),
-    callCreds
-  );
+  const combCreds = grpc.credentials.combineChannelCredentials(grpc.credentials.createSsl(rootCert), callCreds);
 
   // Return pub/sub gRPC client
   const client = new sfdcPackage.PubSub(PUB_SUB_ENDPOINT, combCreds);
@@ -106,44 +97,35 @@ function subscribe(client, topicName, schema) {
     numRequested: PUB_SUB_EVENT_RECEIVE_LIMIT,
   };
   subscription.write(subscribeRequest);
-  console.log(
-    `Subscribe request sent for ${subscribeRequest.numRequested} events from ${topicName}...`
-  );
+  console.log(`Subscribe request sent for ${subscribeRequest.numRequested} events from ${topicName}...`);
 
   // Listen to new events.
-  subscription.on("data", (data) => {
+  subscription.on('data', data => {
     if (data.events) {
       const latestReplayId = data.latestReplayId.readBigUInt64BE();
-      console.log(
-        `Received ${data.events.length} events, latest replay ID: ${latestReplayId}`
-      );
-      const parsedEvents = data.events.map((event) =>
-        parseEvent(schema, event)
-      );
-      console.log(
-        "gRPC event payloads: ",
-        JSON.stringify(parsedEvents, null, 2)
-      );
+      console.log(`Received ${data.events.length} events, latest replay ID: ${latestReplayId}`);
+      const parsedEvents = data.events.map(event => parseEvent(schema, event));
+      console.log('gRPC event payloads: ', JSON.stringify(parsedEvents, null, 2));
     } else {
       // If there are no events then every 270 seconds the system will keep publishing the latestReplayId.
     }
   });
-  subscription.on("end", () => {
-    console.log("gRPC stream ended");
+  subscription.on('end', () => {
+    console.log('gRPC stream ended');
   });
-  subscription.on("error", (err) => {
+  subscription.on('error', err => {
     // TODO: Handle errors
-    console.error("gRPC stream error: ", JSON.stringify(err));
+    console.error('gRPC stream error: ', JSON.stringify(err));
   });
-  subscription.on("status", (status) => {
-    console.log("gRPC stream status: ", status);
+  subscription.on('status', status => {
+    console.log('gRPC stream status: ', status);
   });
 }
 
 /**
  * Publishes a payload to a topic using the gRPC client
  * @param {Object} client Pub/Sub API gRPC client
- * @param {string} topicName name of the topic that we're subscribing to
+ * @param {string} topicName name of the topic that we're publishing to
  * @param {Object} schema event schema associated with the topic
  * @param {string} schema.id
  * @param {Object} schema.type
@@ -157,7 +139,7 @@ async function publish(client, topicName, schema, payload) {
         topicName,
         events: [
           {
-            id: "124", // this can be any string
+            id: '124', // this can be any string
             schemaId: schema.id,
             payload: schema.type.toBuffer(payload),
           },
@@ -169,20 +151,23 @@ async function publish(client, topicName, schema, payload) {
         } else {
           resolve(response);
         }
-      }
+      },
     );
   });
 }
 
 async function run() {
+  // let payload = {
+  //   Account_Name__c: 'Stones Test 1',
+  // };
   try {
     const authParams = await authLib.authenticate(config);
-    console.log("Auth Params: ", authParams);
     const client = connectToPubSubApi(authParams);
     const topicSchema = await getEventSchema(client, PUB_SUB_TOPIC_NAME);
     subscribe(client, PUB_SUB_TOPIC_NAME, topicSchema);
+    // publish(client, PUB_SUB_TOPIC_NAME, topicSchema, payload);
   } catch (err) {
-    console.error("Fatal error: ", err);
+    console.error('Fatal error: ', err);
   }
 }
 
